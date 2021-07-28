a62023c2ffec2e8bbf4099575ce929266c2dfa6d
Mehmet Emin Kartal <kartalbombe@icloud.com>
2021-07-28 23:01:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,75 @@ import logo from './logo.svg';
 import './App.css';
 function b64DecodeUnicode(str) {
   // Going backwards: from bytestream, to percent-encoding, to original string.
-  return decodeURIComponent(atob(str).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  return decodeURIComponent(atob(str).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
 }
 function App() {
+  let xmltohtml = (contents) => {
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(contents, "text/xml");
+    // console.log(xmlDoc);
+    let docs = xmlDoc.getElementsByTagNameNS('*', 'EmbeddedDocumentBinaryObject');
+    let xslt = "";
+
+    for(let i = 0; i < docs.length; i++) {
+      const d = docs[i];
+      let v = d.attributes.filename.value;
+      let ext = v.split('.').pop().toLowerCase();
+      if(ext === "xslt" || ext === "xsl") {
+        xslt = b64DecodeUnicode(d.innerHTML);
+      }
+      console.log(v, ext);
+      // if(d.)
+      window.d = d;
+      console.log(i, d);
+    }
+    // return;
+    // let xslt = b64DecodeUnicode(docs[0].innerHTML);
+    console.log(xslt);
+
+    if(!xslt) {
+      alert("Geçersiz");
+      return;
+    }
+    var xsltProcessor = new XSLTProcessor();
+
+    let parser2 = new DOMParser();
+    let xslStylesheet = parser2.parseFromString(xslt, "text/xml");
+    xsltProcessor.importStylesheet(xslStylesheet);
+    
+    var doc = xsltProcessor.transformToDocument(xmlDoc);
+    
+    console.log(doc);
+    // window.doc = doc;
+    let blob = new XMLSerializer().serializeToString(doc)
+    // console.log(blob);
+    const winUrl = URL.createObjectURL(
+      new Blob([blob], { type: "text/html;charset=utf-8;" })
+
+    );
+
+    var frog = window.open(winUrl,  '_blank'); //,"location=no,menubar=no,scrollbars=no,status=no")
+    frog.print();
+
+    // setTimeout(function () { frog.print(); }, 1);
+    // frog.onfocus = function () { setTimeout(function () { frog.close(); }, 500); }
+    // frog.close();
+    // frog.print();
+  }
+let getXml = (event) => {
+    event.preventDefault();
+    event.target.files[0].text().then(contents => {
+      xmltohtml(contents);
+    });
+}
   let f = (ev) => {
     console.log('File(s) dropped');
 
     // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
-  
+
     if (ev.dataTransfer.items) {
       console.log("f");
       // Use DataTransferItemList interface to access the file(s)
@@ -22,36 +80,13 @@ function App() {
 
           var file = ev.dataTransfer.items[i].getAsFile();
 
-         let reader = new FileReader();
+          let reader = new FileReader();
           reader.onload = function (event) {
             let contents = event.target.result;
+            xmltohtml(contents);
 
-
-            let parser = new DOMParser();
-            let xmlDoc = parser.parseFromString(contents,"text/xml");
-            console.log(xmlDoc);
-            let xslt = b64DecodeUnicode(xmlDoc.getElementsByTagName('cbc:EmbeddedDocumentBinaryObject')[0].innerHTML);
-            console.log(xslt);
-
-
-            var xsltProcessor = new XSLTProcessor();
-
-            let parser2 = new DOMParser();
-            let xslStylesheet = parser2.parseFromString(xslt,"text/xml");
-            xsltProcessor.importStylesheet(xslStylesheet);
-            var doc = xsltProcessor.transformToDocument(xmlDoc);
-            console.log(doc);
-            // window.doc = doc;
-            let blob = new XMLSerializer().serializeToString(doc)
-            console.log(blob);
-          const winUrl = URL.createObjectURL(
-            new Blob(["\ufeff", blob], { type: "text/html;charset=utf-8;" })
-        );
-
-          var frog = window.open(winUrl)
-          frog.print();
             //holder.style.background = 'url(' + event.target.result + ') no-repeat center';
-        
+
           };
           console.log(file);
           reader.readAsText(file);
@@ -80,14 +115,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <label
           className="App-link"
+          htmlFor={"choseFile"}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </label>
+        <input id="choseFile" onChange={getXml} type='file' style={{ display: 'none' }} accept="text/xml" />
       </header>
     </div>
   );
