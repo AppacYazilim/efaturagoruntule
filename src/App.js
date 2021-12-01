d72535bdbf9a556a4b9a3038d3e63da81d0891a2
Mehmet Emin Kartal <mehmet@appac.ltd>
2021-12-02 00:07:16 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -21,9 +21,13 @@ function App() {
         {/* <input id="choseFile" onChange={getXml} type='file' style={{ display: 'none' }} accept="text/xml" /> */}
         <StyledDropZone />
       </header>
-      <footer className={"text-muted"}>
-        2021 &copy;	Appac Yazılım Elektronik San. Tic. Ltd. Şti. <br />
-        Tüm hakları saklıdır. Verileriniz hiçbir şekilde sunucularımıza iletilmemektedir. 
+
+      <footer className={"text-muted container text-small"}>
+        <small>
+        &copy; 2021 <a href={"https://appac.ltd"} rel="noreferrer" target={"_blank"}>Appac Yazılım Elektronik San. Tic. Ltd. Şti. </a><br />
+        Tüm hakları saklıdır. Verileriniz hiçbir şekilde sunucularımıza iletilmemektedir. HİÇ BİR ŞEKİLDE SİSTEMİN ÇALIŞMASI ÜZERİNE GARANTİ VERİLMEMEKTİR! TAMAMEN REFERANS AMAÇLI KULLANILMALIDIR. SİSTEM MALİ MÜHÜR DOĞRULAMASI YAPMAMAKTADIR! 
+        Appac Yazılım Elektronik San. Tic. Ltd. Şti. SİSTEMİN ÇALIŞMAMASI VEYA YANLIŞ ÇALIŞMASI DURUMUNDA HİÇ BİR SORUMLULUK KABUL ETMEZ. SİSTEME DOSYA YÜKLEMEK İLE BU KOŞULLARI KABUL ETTİĞİNİZ VARSAYILIR. 
+        </small>
       </footer>
       <ToastContainer />
     </div>
